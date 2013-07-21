a55c53ae6cc1754a13fa744751031cb79f66f26f
Mark Wotton <mwotton@gmail.com>
2013-07-21 13:16:36 +1000

    add working suffix tree implementation

--- Data/List/CommonSubstring.hs
+++ Data/List/CommonSubstring.hs
@@ -1,4 +1,23 @@
 module Data.List.CommonSubstring where
+import Data.SuffixTree
+import Data.List
+import Data.Ord
 
 longestSubstring ::(Eq a, Ord a) => [a] -> [a] -> [a]
-longestSubstring first second = error "unimplemented"
+longestSubstring first second = head $ reverse $ sortBy (comparing length)
+                                     $ map (longestMatch $ construct second)
+                                     $ tails first
+  where longestMatch :: Eq a => STree a -> [a] -> [a]
+        longestMatch Leaf _ = []
+        longestMatch (Node edges) candidate =
+          head $ reverse $ sortBy (comparing length) $ map (prefixMatch candidate) edges
+
+        prefixMatch :: Eq a => [a] -> Edge a -> [a]
+        prefixMatch candidate (p, tree)
+          | p' `isPrefixOf` candidate = p' ++ longestMatch tree (drop (length p') candidate)
+          | otherwise = commonSubstring p' candidate
+          where p' = prefix p
+        commonSubstring (a:as) (b:bs)
+          | a==b = a:(commonSubstring as bs)
+          | otherwise = []
+        commonSubstring _ _ = []
