ff72959f8ff8ffdeb57023dd1a510a6582b99bbc
Mark Wotton <mwotton@gmail.com>
2013-07-21 14:01:34 +1000

    benches

--- bench/BenchAll.hs
+++ bench/BenchAll.hs
@@ -4,17 +4,17 @@ import Criterion.Main
 import qualified Data.List.CommonSubstring as Suffix
 import qualified Data.List.SlowSubstring as Slow
 
-padlen = 1000
-
-first = padding ++ "bbbbbbbb" ++ padding
+first padlen = padding ++ "bbbbbbbb" ++ padding
   where  padding = concat $ replicate padlen  "abcd"
 
-second = padding ++ "bbbbbbb" ++ padding
+second padlen = padding ++ "bbbbbbb" ++ padding
   where  padding = concat (replicate padlen "efgh")
 
 main = defaultMain [
         bgroup "string-similarity"
-          [ --bench "slow" $ whnf (Slow.longestSubstring first) second
-            bench "common" $ whnf (Suffix.longestSubstring first) second
-          ]
+         [ bench "slow" $ whnf (Slow.longestSubstring $ first 100) (second 100),
+           bench "common" $ whnf (Suffix.longestSubstring $ first 100) (second 100),
+           bench "common" $ whnf (Suffix.longestSubstring $ first 1000) (second 1000),
+           bench "common" $ whnf (Suffix.longestSubstring $ first 10000) (second 10000)
+         ]
         ]
