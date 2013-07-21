0a7978fe31c3c8ffd0157d4c990f925de681a719
Mark Wotton <mwotton@gmail.com>
2013-07-21 13:48:34 +1000

    hlint suggestions

--- Data/List/CommonSubstring.hs
+++ Data/List/CommonSubstring.hs
@@ -4,13 +4,13 @@ import Data.List
 import Data.Ord
 
 longestSubstring ::(Eq a, Ord a) => [a] -> [a] -> [a]
-longestSubstring first second = head $ reverse $ sortBy (comparing length)
+longestSubstring first second = maximumBy (comparing length)
                                      $ map (longestMatch $ construct second)
                                      $ tails first
   where longestMatch :: Eq a => STree a -> [a] -> [a]
         longestMatch Leaf _ = []
         longestMatch (Node edges) candidate =
-          head $ reverse $ sortBy (comparing length) $ map (prefixMatch candidate) edges
+          maximumBy (comparing length) $ map (prefixMatch candidate) edges
 
         prefixMatch :: Eq a => [a] -> Edge a -> [a]
         prefixMatch candidate (p, tree)
@@ -18,6 +18,6 @@ longestSubstring first second = head $ reverse $ sortBy (comparing length)
           | otherwise = commonSubstring p' candidate
           where p' = prefix p
         commonSubstring (a:as) (b:bs)
-          | a==b = a:(commonSubstring as bs)
+          | a==b = a:commonSubstring as bs
           | otherwise = []
         commonSubstring _ _ = []
